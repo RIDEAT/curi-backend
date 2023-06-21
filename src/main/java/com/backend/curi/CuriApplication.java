64536443c343591d908a444c860c56d3b2d18eb4
류지승 <8514199@gmail.com>
2023-06-21 21:32:26 +0900

    first commit

---
+++ src/main/java/com/backend/curi/CuriApplication.java
@@ -0,0 +1,13 @@
+package com.backend.curi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/curi/CuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.curi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
