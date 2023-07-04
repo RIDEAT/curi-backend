6959536504ff4ec6f871a632762028031891805d
POPORION <black4roach@gmail.com>
2023-07-04 17:25:45 +0900

    [RID-121][#25][feat] : add test resources for build process
    아마존 rds에 test 과정에서 연결할 수 없기에 test 환경에서는 h2 db 쓰도록 수정

--- src/test/java/com/backend/curi/CuriApplicationTests.java
+++ src/test/java/com/backend/curi/CuriApplicationTests.java
@@ -2,7 +2,9 @@ package com.backend.curi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(locations = "/application-data.properties")
 @SpringBootTest
 class CuriApplicationTests {
 
