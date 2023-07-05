16bc28338014a6e9e7489116243cbb024df94f74
POPORION <black4roach@gmail.com>
2023-07-05 16:54:36 +0900

    [RID-206][#42][chore] : modify FirebaseConfig.java to read json file
    FileInputStream은 파일시스템의 절대 경로를 기준으로 파일을 찾음,
    프로젝트의 클래스패스로 읽어와야하므로 해당 작업을 수행하도록 수정

--- src/main/java/com/backend/curi/firebase/FirebaseConfig.java
+++ src/main/java/com/backend/curi/firebase/FirebaseConfig.java
@@ -4,21 +4,23 @@ package com.backend.curi.firebase;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
 import com.google.firebase.FirebaseOptions;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
+import org.springframework.core.io.ClassPathResource;
+
 @Configuration
 public class FirebaseConfig {
 
     @PostConstruct
     public void init(){
         try{
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/serviceAccountKey.json");
+            GoogleCredentials credentials = GoogleCredentials.fromStream(new ClassPathResource("serviceAccountKey.json").getInputStream());
+
             FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(credentials)
                     .build();
+
             FirebaseApp.initializeApp(options);
         }catch (Exception e){
             e.printStackTrace();
