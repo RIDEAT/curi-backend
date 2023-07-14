418b5892ce2b994755cf320841a513038e79f341
POPORION <black4roach@gmail.com>
2023-07-14 22:50:29 +0900

    [RID-202][#79][feat] : fix swagger cors error

--- src/main/java/com/backend/curi/CuriApplication.java
+++ src/main/java/com/backend/curi/CuriApplication.java
@@ -1,8 +1,11 @@
 package com.backend.curi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url="/", description = "Default Server Url")})
 @SpringBootApplication
 public class CuriApplication {
 
