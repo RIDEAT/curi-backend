0fa0c1c0c3f65bd29a6f482417d81248564fa672
류지승 <8514199@gmail.com>
2023-09-01 15:42:17 +0900

    append log

--- src/main/java/com/backend/curi/common/Common.java
+++ src/main/java/com/backend/curi/common/Common.java
@@ -1,12 +1,21 @@
 package com.backend.curi.common;
 
+import com.backend.curi.exception.CuriException;
 import com.backend.curi.security.dto.CurrentUser;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Common {
     public CurrentUser getCurrentUser(){
-        return (CurrentUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        try {
+            return (CurrentUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        } catch (NullPointerException e) {
+            CurrentUser currentUser = new CurrentUser();
+            currentUser.setUserId("no-cuurent-user");
+            currentUser.setNewAuthToken("no-cuurent-user");
+            return currentUser;
+        }
     }
 }
