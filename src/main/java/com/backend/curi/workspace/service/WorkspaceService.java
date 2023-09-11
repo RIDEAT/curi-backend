becff47d8ee73e8e6f9108e3ad976e6bb1529146
POPORION <black4roach@gmail.com>
2023-09-12 02:18:50 +0900

    delete default workflow

--- src/main/java/com/backend/curi/workspace/service/WorkspaceService.java
+++ src/main/java/com/backend/curi/workspace/service/WorkspaceService.java
@@ -74,7 +74,7 @@ public class WorkspaceService {
         userworkspaceService.create(currentUser, savedWorkspace);
 
         createDefaultRole(savedWorkspace);
-        createDefaultWorkflow(savedWorkspace);
+        //createDefaultWorkflow(savedWorkspace);
         slackService.sendMessageToRideat(new SlackMessageRequest("새로운 워크스페이스가 생성되었습니다. 이름 : " + request.getName() + ", 유저: "+ currentUser.getUserId()));
 
         return savedWorkspace;
