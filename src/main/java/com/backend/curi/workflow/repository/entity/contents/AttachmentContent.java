e7a33a7ba445187d76b26151d687ce96b0a3273d
POPORION <black4roach@gmail.com>
2023-10-23 19:48:33 +0900

    fix build error

--- src/main/java/com/backend/curi/workflow/repository/entity/contents/AttachmentContent.java
+++ src/main/java/com/backend/curi/workflow/repository/entity/contents/AttachmentContent.java
@@ -1,16 +1,15 @@
 package com.backend.curi.workflow.repository.entity.contents;
 
 import com.backend.curi.reports.AttachmentsInfo;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AttachmentContent {
     @Builder.Default
     private List<AttachmentsInfo> attachments = new ArrayList<>();
