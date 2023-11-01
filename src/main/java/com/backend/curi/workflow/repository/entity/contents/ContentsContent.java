6724fa8a4d896f46e256f05820e9d3b668ec7488
POPORION <black4roach@gmail.com>
2023-11-01 18:24:26 +0900

    modify content patch api to inject map data

--- src/main/java/com/backend/curi/workflow/repository/entity/contents/ContentsContent.java
+++ src/main/java/com/backend/curi/workflow/repository/entity/contents/ContentsContent.java
@@ -5,10 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Map;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ContentsContent {
-    private Object content;
+    private Map<String, Object> content;
 }
